94fe64fa216a6cc4c9d8f463456fd6f31a7cab43
renebec <39104197+renebec@users.noreply.github.com>
2026-07-06 21:14:16 -0600

    Fix typo in subheader for graduation percentage

--- app.py
+++ app.py
@@ -67,7 +67,7 @@ with tab1:
     *   **$MN_{gt}$**: Matrícula de nuevo ingreso en el ciclo (t-2).
     """)
     
-    st.subheader("5. % de egresados con promeidio >= 8 en el ciclo (t)")
+    st.subheader("5. % de egresados con promedio >= 8 en el ciclo (t)")
     st.write("Mide la razón de egresados que obtuvieron promedio >= 8 en el ciclo t:")
     st.latex(r"E_{gr8} = \frac{ E_{pt8} }{E_{t8}} * 100")
     st.markdown("""
